0f98b316f31f8cfea813015fe7ad94d756610155
Heather Walker <cookiesncream@ladle.net>
2016-02-23 11:45:14 -0800

    first commit

---
+++ lib/cool_gem.rb
@@ -0,0 +1,5 @@
+require "cool_gem/version"
+
+module CoolGem
+  # Your code goes here...
+end
---
+++ spec/cool_gem_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe CoolGem do
+  it 'has a version number' do
+    expect(CoolGem::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
